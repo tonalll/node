a2ae8f4054429d336cc9fa2f21f0e9891fdd182b
tonalll <tonalll@qq.com>
2016-03-27 23:49:19 +0800

    a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//index.js
+var test = require('./test');
+test.setName('bbb');
+test.printName();
+test.setName('ccc');
+test.printName();
+
+var http = require("http");
+http.createServer(function (request, response) {
+    console.log('request received');
+    response.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    response.write(index.html);
+    response.end();
+}).listen(8888);
+console.log('server started');
--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-asfd
+var name = 'aaa';
+
+function setNmae(_name) {
+    name = _name;
+}
+
+function printName() {
+    console.info(name);
+}
+exports.setName = setNmae;
+exports.printName = printName;
